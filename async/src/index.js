08b8d991a87009503e19a47b626f58b6fe72ef94
Jay <imjching@users.noreply.github.com>
2017-05-27 15:23:11 -0700

    sample async reddit app

--- async/src/index.js
+++ async/src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import reducer from './reducers'
+import App from './containers/App'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
