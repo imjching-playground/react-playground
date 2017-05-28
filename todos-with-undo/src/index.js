b83f775b6d32825eb52882ee0fe2da4a95e5e76a
Jay <imjching@users.noreply.github.com>
2017-05-28 00:57:01 -0700

    more stuff

---
+++ todos-with-undo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
